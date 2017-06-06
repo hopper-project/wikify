a707782716e79c3e94a2b716a0aa371508755ec2
tophatraptor <jsdhanoa987@gmail.com>
2017-06-06 14:18:33 -0500

    resovlved makedir issue

--- extractor.py
+++ extractor.py
@@ -116,7 +116,8 @@ def main():
     args = parser.parse_args()
     input_path = args.input_path
     output_path = args.output_path
-    os.makedirs(output_path)
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
     allfiles = os.listdir(input_path)
     allpaths = map(lambda x: os.path.join(input_path, x), allfiles)
     pool = mp.Pool(processes = mp.cpu_count())

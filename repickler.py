97fad66642d30f712cb4bd38cf60a2b04f56b344
tophatraptor <jsdhanoa987@gmail.com>
2017-06-13 23:05:11 -0500

    updated with argparse/documentation

--- repickler.py
+++ repickler.py
@@ -2,35 +2,40 @@ import sys
 import os
 import cPickle as pickle
 
-def main(data_path, output_path):
+def main():
+    parser = argparse.ArgumentParser(
+    description='Generates topranks.tsv file from ranks.p file'
+    )
+    parser.add_argument('input_path',
+    help='Path to directory containing ranks.p')
+
+    parser.add_argument('output_path',
+    help='Path to topranks.tsv output directory')
+
+    args = parser.parse_args()
+    data_path = args.input_path
+    output_path = args.output_path
+
     rank_path = os.path.join(data_path, 'ranks.p')
-    rank_handle = open(rank_path, 'rb')
-    ranks = pickle.load(rank_handle)
-    rank_handle.close()
+    with open(rank_path, 'rb') as fh:
+        ranks = pickle.load(fh)
 
     tsv_path = os.path.join(output_path, 'topranks.tsv')
-    tsv_handle = open(tsv_path, 'a')
-
-    counter = 0
-    for key in ranks.keys():
-        if not counter == 0:
-            topchoice = ranks[key][0]
-            toptitle = topchoice[0]
-            topfreq = topchoice[1]
-            tsv_handle.write("{}\t{}\t{}\n".format(key, toptitle, topfreq))
-            if counter % 1000 == 0:
-                print(counter)
-        counter += 1
-
-    tsv_handle.close()
-
+    with open(tsv_path, 'w') as fh:
+        counter = 0
+        for key in ranks.keys():
+            if not counter==0:
+                topchoice = ranks[key][0]
+                toptitle = topchoice[0]
+                topfreq  = topchoice[1]
+                fh.write("{}\t{}\t{}\n".format(key, toptitle, topfreq))
+                if counter % 1000 == 0:
+                    print(counter)
+            counter += 1
     sys.exit(0)
 
-if __name__ == '__main__':
-    if len(sys.argv) == 3:
-        input_path = sys.argv[1]
-        output_path = sys.argv[2]
-    else:
-        print("argument count error")
-        sys.exit(1)
-    main(input_path, output_path)
+if sys.flags.interactive:
+    pass
+else:
+    if __name__=='__main__':
+        main()
